e778bcc83d76d265dd46cf41fce88bdb3a52ae02
Julius Kanani <juliusm.kanani@gmail.com>
2022-03-21 17:17:22 +0300

    Directory structure and a README.md file created. Task 0 has been solved.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value it points to, to 98.
+ * @n: a pointer to a variable whose value will be updated to 98.
+ * Return: type is void. returns nothing.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,5 @@
+## 0x05-pointers\_arrays\_strings.
+| Task | File name | Task Description. |
+| --- | --- | --- |
+| Task 0. | 0-reset\_to\_98.c | Function that takes a pointer to an int as parameter and updates the value it points to, to 98. |
+
