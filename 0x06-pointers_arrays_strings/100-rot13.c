21de8945dd0b5368c29d9310b23c91491f9e4fd7
Julius Kanani <juliusm.kanani@gmail.com>
2022-03-25 21:55:30 +0300

    8. rot13 - encodes a stringusing rot 13.

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * *rot13 - encodes a string.
+ * @s: ptr to char to be encoded.
+ * Return: Always 0.
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+	char r1[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+                     'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+                     'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+                     'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+                     'Z'};
+        char r2[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+                     'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+                     'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+                     'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+                     'M'};
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (j < 53)
+		{
+			if (r1[j] == s[i])
+			{
+				s[i] = r2[j];
+				break;
+			}
+			++j;
+		}
+		++i;
+	}
+
+	return (s);
+}
