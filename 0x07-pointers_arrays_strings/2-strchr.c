e3f5ba6d81e0d5fdda32f1b57aaf2beefb95fa6f
Julius Kanani <juliusm.kanani@gmail.com>
2022-03-28 19:36:50 +0300

    2. *_strchr - locates a character in a string and returns a ptr for the first occurrence of char c in str.

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * *_strchr - locates a character in a string.
+ * @s: string for character locate operation.
+ * @c: character to be located in s.
+ * Return: ptr to the first occurrence of the character c in s. NULL if
+ * the character is not found.
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+	char *ptr;
+
+	i = 0;
+	ptr = NULL;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			ptr = &s[i];
+			break;
+		}
+		++i;
+	}
+	
+	return (ptr);
+}
