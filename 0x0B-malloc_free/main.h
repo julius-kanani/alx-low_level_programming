2a80b987810b783ad7f30ca43e3198d07f35c147
Julius Kanani <juliusm.kanani@gmail.com>
2022-04-06 04:50:16 +0300

    Function that frees a 2 dimensional grid.

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,13 @@
+#ifdef MAIN_H
+#define MAIN_H
+
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *, char *);
+int **alloc_grid(int, int);
+void free_grid(int **, int);
+char *argstostr(int, char **);
+char **strtow(char *);
+int _putchar(char);
+
+#endif
