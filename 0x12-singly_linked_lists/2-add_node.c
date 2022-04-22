8282c4c54e9360494b5456a036b20620e1ee236e
Julius Kanani <juliusm.kanani@gmail.com>
2022-04-22 17:32:43 +0300

    Task 2- add node correction.

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,41 +1,49 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
 /**
-  *_strlen - finds length of a string.
-  *@str: string.
-  *
-  *Return: lengt of string.
-  */
-int _strlen(const char *str)
+ * _strlen - find the length of a string.
+ * @str: string to find its length.
+ * Return: length of a string.
+ */
+
+size_t _strlen(const char *str)
 {
-	int i;
+	int strl;
 
-	while (str[i] != '\0')
-		i++;
+	strl = 0;
+	while (*str != '\0')
+	{
+		++strl;
+		++str;
+	}
 
-	return (i);
+	return (strl);
 }
 
 /**
-  *add_node - adds a new node at the beginning of a list_t.
-  *@head: pointer to first element of the list.
-  *@str: string to be duplicated.
-  *
-  *Return: address of the new element or NULL if it failed.
-  */
+ * add_node - adds a new node to the beginning of a (list_t) list.
+ * @head: a ptr to the head of a list.
+ * @str: str to be be added to the linked list.
+ * Return: address of the new Linked_list.
+ */
+
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *temp;
+	list_t *temp_head;
 
-	temp = malloc(sizeof(list_t));
-	if (temp == NULL)
+	temp_head = malloc(sizeof(list_t));
+
+	if (temp_head == NULL || str == NULL)
 		return (NULL);
-	/* temp now points to first element*/
-	temp->next = *head;
-	temp->str = strdup(str);
-	temp->len = _strlen(str);
 
-	*head = temp;
+	/** fill temp_head */
+	temp_head->str = strdup(str);
+	temp_head->len = _strlen(str);
+	temp_head->next = *head;
+
+	*head = temp_head;
 
 	return (*head);
-
 }
