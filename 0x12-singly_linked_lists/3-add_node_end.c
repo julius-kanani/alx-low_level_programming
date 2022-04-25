604570c99706d51643d230a411b822e71282483e
Julius Kanani <juliusm.kanani@gmail.com>
2022-04-25 04:11:20 +0300

    add node at the end of a list.

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,60 +1,54 @@
 #include "lists.h"
-#include <stdlib.h>
-#include <string.h>
 
 /**
- * _strlen - find the length of a string.
- * @str: string to find its string.
- * Return: length of a string.
- */
-
-size_t _strlen(const char *str)
+  *_strlen - returns length of string.
+  *@str: string;
+  *
+  *Return: length of string.
+  */
+  
+int _strlen(const char *str)
 {
-	int strl;
+	int i;
 
-	strl = 0;
-	while (*str != '\0')
-	{
-		++strl;
-		++str;
-	}
+	i = 0;
+	while (str[i] != '\0')
+		i++;
 
-	return (strl);
+	return (i);
+
 }
 
 /**
- * add_node_end - adds a new node at end of a (list_t) list.
- * @head: address of the head of a (list_t) list.
- * @str: string to be added to node of the (list_t) list.
- * Return: address of the new linked list.
- */
-
+  *add_node_end - adds a new node at the end of a list_t.
+  *@head: pointer to head element.
+  *@str: string to be duplicated
+  *
+  *Return: address of the new element.
+  */
+  
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node = NULL, *prev_node = NULL;
+	list_t *newNode, *lastNode;
 
-	new_node = (list_t *) malloc(sizeof(list_t));
-
-	if (new_node == NULL)
+	newNode = malloc(sizeof(list_t));
+	if (newNode == NULL)
 		return (NULL);
 
-	new_node->str = strdup(str);
-	new_node->len = _strlen(str);
-	new_node->next = NULL;
+	newNode->str = strdup(str);
+	newNode->len = _strlen(str);
+	newNode->next = NULL;
 
-	if (*head == NULL)  /* if the list has no beginning */
-		*head = new_node;
-	else    /* if the list has a beginning */
+	if (*head == NULL)
+		*head = newNode;
+	else
 	{
-		prev_node = *head;  /* assign the given address to the previous list node */
+		lastNode = *head;
 
-		while (prev_node->next != NULL)
-		{	/* while the previous node has a link to the next node */
-			prev_node = prev_node->next;
-		}
-
-		prev_node->next = new_node; /* assign it as the last node */
+		while (lastNode->next != NULL)
+			lastNode = lastNode->next;
+		lastNode->next = newNode;
 	}
 
-	return (new_node);
+	return (newNode);
 }
