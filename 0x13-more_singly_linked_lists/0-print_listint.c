e50a7ce677f1f2879bb6a535f6f11046538635c0
Julius Kanani <juliusm.kanani@gmail.com>
2022-04-25 12:09:13 +0300

    Task 0 - print_listint.c - prints all the elements of a (listint_t) list.

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -13,16 +13,16 @@ size_t print_listint(const listint_t *h)
 	unsigned int num_of_nodes;
 
 	num_of_nodes = 0;
-	if (h != NULL)
+	if (h != NULL)	/* is the address given */
 	{
-		while (h->next != NULL)
+		while (h->next != NULL)	/* curr node has next address */
 		{
 			printf("%d\n", h->n);
-			++num_of_nodes;
-			h = h->next;
+			++num_of_nodes;	/* increment number of nodes */
+			h = h->next;	/* go to next node */
 		}
-		printf("%d\n", h->n);
-		++num_of_nodes;
+		printf("%d\n", h->n);	/* print last node */
+		++num_of_nodes;	/* add last node */
 	}
 
 	return (num_of_nodes);
