0b2367b3b242b6927bffba654c182bad337c6d70
Julius Kanani <juliusm.kanani@gmail.com>
2022-04-26 22:38:50 +0300

    Task 10. Delete at index - delete_nodeint_at_index - deletes the node at a given index of a linked list.

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 
 /**
-  * delete_nodeint_at_index - deletes the node at index.
-  * @head: pointer to pointer of first elemnt of list.
-  * @index: index of node to be deleted.
-  *
-  * Return: 1 if successful, -1 if fails.
-  */
-  
+ * delete_nodeint_at_index - deletes the node at index.
+ * @head: pointer to pointer of first elemnt of list.
+ * @index: index of node to be deleted.
+ *
+ * Return: 1 if successful, -1 if fails.
+ */
+
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int count;
