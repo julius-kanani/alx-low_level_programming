dc84d03402807d9aa9b452e5a0ae92554e6927b2
Julius Kanani <juliusm.kanani@gmail.com>
2022-05-04 02:21:48 +0300

    Task 1 - create_file - creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,5 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
   * create_file - creates a file.
   * @filename: pointer to filename.
@@ -6,6 +7,7 @@
   *
   * Return:  1 on success -1 on failure.
   */
+
 int create_file(const char *filename, char *text_content)
 {
 	int file, i, wr;
