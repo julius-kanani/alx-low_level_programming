1ff45e75c3baa0fa0484e9d70c2c1139219b5216
Julius Kanani <juliusm.kanani@gmail.com>
2022-06-09 21:42:30 +0300

    Task 2 - lists.h header file updated, add node

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,6 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stddef.h>
+
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -22,5 +24,8 @@ size_t print_dlistint(const dlistint_t *h);
 
 /* returns the number of elements in a linked dlistint_t list. */
 size_t dlistint_len(const dlistint_t *h);
+
+/* adds a new node at the beginning of a dlistint_t list */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 
 #endif /* LISTS_H */
