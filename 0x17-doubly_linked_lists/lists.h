b10a3ac0ee1f2c5ad993d8cf76dc0c5b17c3416b
Julius Kanani <juliusm.kanani@gmail.com>
2022-06-10 10:10:38 +0300

    task 5 - lists.h header file updated, get node at a given index

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -35,4 +35,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 /* frees a dlistint_t list */
 void free_dlistint(dlistint_t *head);
 
+/* returns the nth node of a dlistint_t linked list */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+
 #endif /* LISTS_H */
