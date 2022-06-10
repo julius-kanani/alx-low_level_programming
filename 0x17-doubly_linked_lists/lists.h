74434fd5b675c667adf90fefdc5dae841eb41aa0
Julius Kanani <juliusm.kanani@gmail.com>
2022-06-10 12:26:10 +0300

    Task 8 - lists.h header file updated, Delete at index

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -44,4 +44,7 @@ int sum_dlistint(dlistint_t *head);
 /* inserts a new node at a given position. */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 
+/* deletes the node at the given index of a dlistint_t linked list */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+
 #endif /* LISTS_H */
