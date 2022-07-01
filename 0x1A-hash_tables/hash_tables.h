7f8718b15162c43588831f565575738a71747998
Julius M. Kanani <juliusm.kanani@gmail.com>
2022-07-01 22:04:05 +0300

    Task 1 - header file

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -38,4 +38,7 @@ typedef struct hash_table_s
 /* Creates a hash table */
 hash_table_t *hash_table_create(unsigned long int size);
 
+/* hash function implementing the djb2 algorithm */
+unsigned long int hash_djb2(const unsigned char *str);
+
 #endif /* HASH_TABLES_H */
