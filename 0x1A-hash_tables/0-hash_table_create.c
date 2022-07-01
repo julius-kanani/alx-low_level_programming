0c9cc010fe8e70bb9dadb7941038050c0992406c
Julius M. Kanani <juliusm.kanani@gmail.com>
2022-07-01 21:55:50 +0300

    task 0 - used malloc instead of calloc

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,16 +8,19 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
+	unsigned int i;
 
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
 
-	ht->array = calloc(size, sizeof(hash_node_t *));
+	ht->array = malloc(size * sizeof(hash_node_t *));
 	if (ht->array == NULL)
 		return (NULL);
-
 	ht->size = size;
+
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
 
 	return (ht);
 }
