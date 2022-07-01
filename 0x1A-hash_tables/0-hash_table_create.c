020cb369d1dd7eb6167889cf3c23c62fa7a3772d
Julius M. Kanani <juliusm.kanani@gmail.com>
2022-07-01 21:18:09 +0300

    Task 0 - create a hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: Size of the hash table
+ * Return: A pointer to the newly created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+
+	ht->array = calloc(size, sizeof(hash_node_t *));
+	if (ht->array == NULL)
+		return (NULL);
+
+	ht->size = size;
+
+	return (ht);
+}
+
