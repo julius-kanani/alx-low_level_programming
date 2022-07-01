aec244a8c9a03c1c3489bb39d92d17a74215d158
Julius M. Kanani <juliusm.kanani@gmail.com>
2022-07-01 23:21:51 +0300

    task 5 - header file, print a hash table

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -54,4 +54,7 @@ hash_node_t *make_hash_node(const char *key, const char *value);
 /* retrieves a value associated with a key*/
 char *hash_table_get(const hash_table_t *ht, const char *key);
 
+/* prints a hash table */
+void hash_table_print(const hash_table_t *ht);
+
 #endif /* HASH_TABLES_H */
