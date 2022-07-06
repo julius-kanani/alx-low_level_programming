917dec0a3a801ad5262cea1cad9907d394625884
Julius M. Kanani <juliusm.kanani@gmail.com>
2022-07-06 16:50:12 +0300

    Task 6 - hash_table header file updated

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -57,4 +57,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key);
 /* prints a hash table */
 void hash_table_print(const hash_table_t *ht);
 
+/* Deletes a hash table */
+void hash_table_delete(hash_table_t *ht);
+
 #endif /* HASH_TABLES_H */
